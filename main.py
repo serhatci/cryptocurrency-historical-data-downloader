3f1bdccfd2eed11ec2c74a49607e0073fa09d068
serhat <serhatciftci@gmail.com>
2020-10-13 01:04:14 +0200

    Refactor display method

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def run(app):
     # Creates the start screen layout
     layout = Layout.create(app.exc_list)
 
-    # Initializes start screen window object
-    window = sg.Window('Crypto-exchanges Data Downloader',
-                       layout,
-                       size=(1000, 500),
-                       finalize=True)
+    # Initializes start screen attaches it to app.view
+    app.view.window = sg.Window('Crypto-exchanges Data Downloader',
+                                layout,
+                                size=(1000, 500),
+                                finalize=True)
 
     # Listens the screen and collect user inputs
     while True:
-        event, values = window.read()
+        event, values = app.view.window.read()
 
         # Terminate app when user closes window or clicks cancel
         if event == sg.WIN_CLOSED or event == 'Cancel':
@@ -36,45 +36,50 @@ def run(app):
         if event == '-exchanges_table-':
             col_num = values['-exchanges_table-'][0]
             app.selected_exc = app.exc_list[col_num]
-            app.warning.exc_info(window, app.selected_exc)
+            msg = f'{app.selected_exc.name}\n-----\n' \
+                f'{app.selected_exc.website}'
+            app.display(msg, 'green')
 
         # User clicks -add- button and a new coin is added
         if event == '-add_coin-':
             if not values['-exchanges_table-']:
-                app.warning.select_exc(window)
+                app.display('*Select Exchange')
             else:
                 if not values['-coin_name-'] and values['-abbr-']:
-                    app.warning.missing_info(window)
+                    app.display('*Missing Info')
                 else:
-                    app.add_coin(window)
+                    coin_name = values['-coin_name-']
+                    abbr = values['-abbr-']
+                    start_date = values['-start_date-']
+                    start_hour = values['-start_hour-']
+                    app.add_coin(coin_name, abbr, start_date, start_hour)
 
         # User clicks update button and data starts to download
         if event == '-update_coin-':
             if not values['-coins_table-']:
-                app.warning.select_coin(window)
-
+                app.display('*Select Coin')
             else:
                 col_num = values['-coins_table-'][0]
                 app.selected_coin = app.selected_exc.coin_list[col_num]
-                app.download_data(window, app.selected_coin)
+                app.download_data(app.selected_coin)
 
         # User clicks update all button and all data starts to download
         if event == '-update_all-':
             for coin in app.selected_exc.coin_list:
-                app.download_data(window, coin)
+                app.download_data(coin)
 
         # User clicks delete button and coin data is deleted
         if event == '-delete_coin-':
             if not values['-coins_table-']:
-                app.warning_select_coin(window)
+                app.display('*Select Coin')
             else:
                 col_num = values['-coins_table-'][0]
                 app.selected_coin = app.selected_exc.coin_list[col_num]
-                app.delete_coin(window, app.selected_coin)
+                app.delete_coin(app.selected_coin)
 
-    window.close()
+    app.view.window.close()
 
 
 if __name__ == "__main__":
-    app = Controller()
+    app = Controller(Model(), View())
     run(app)
