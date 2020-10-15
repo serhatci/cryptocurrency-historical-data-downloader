e6290f400c5f53de4c8e637d203e4c9d7f3ce36a
serhat <serhatciftci@gmail.com>
2020-10-15 12:11:44 +0200

    Add docstrings

--- config_cls.py
+++ config_cls.py
@@ -1,13 +1,29 @@
+"""Provides congifuration class.
+
+    List of classes:
+        Config
+    """
 import os
 from sys import platform
 import configparser
 
 
 class Config:
+    """Configuration class to keep default values in a file.
+
+    Attr:
+        __config (object) = python configparser object
+    """
 
     __config = configparser.ConfigParser()
 
     def __init__(self):
+        """Constructor of config class.
+
+        Attr:
+            __platform (str) : operating system
+            __folder_path (str) : save folder path
+        """
 
         self.__config_file_check()
         self.__config.read('config.ini')
@@ -16,21 +32,32 @@ class Config:
 
     @classmethod
     def __config_file_check(cls):
+        """Creates config.ini file if not exists.
+        """
         if not os.path.isfile('config.ini'):
             cls.__create_config_file()
 
     @classmethod
     def __create_config_file(cls):
+        """Creates config.ini file with default values.
+        """
         cls.__config['DEFAULT'] = {'Platform': platform,
                                    'SaveFolder': os.getcwd()}
         cls.__write_config_file()
 
     @classmethod
     def change_folder_path(cls, path):
+        """Changes save folder path and save into config.ini
+
+        Args:
+            path (str): New folder path
+        """
         cls.__config['DEFAULT']['SaveFolder'] = path
         cls.__write_config_file()
 
     @classmethod
     def __write_config_file(cls):
+        """Save __config obj to config.ini
+        """
         with open('config.ini', 'w') as configfile:
             cls.__config.write(configfile)
