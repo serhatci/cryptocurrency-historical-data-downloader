18ba8978f7e31d74c3373822deb9cb813ba8ad04
serhat <serhatciftci@gmail.com>
2020-10-16 12:10:55 +0200

    Remove instantiation of config to model

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from config_cls import Config
 from model_view_controller import Controller, Model, View
 
 if __name__ == "__main__":
     """Executes the application.
     """
     # Instantiate application object
-    app = Controller(Model(Config()), View())
+    app = Controller(Model(), View())
     app.start()
