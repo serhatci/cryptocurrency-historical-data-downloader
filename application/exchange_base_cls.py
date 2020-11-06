08042e43269fbfab265a55c30a993d756f5c19f6
serhat <serhatciftci@gmail.com>
2020-11-06 18:42:18 +0100

    Correct docstring

--- application/exchange_base_cls.py
+++ application/exchange_base_cls.py
@@ -115,10 +115,7 @@ class Exchange(ABC):
 
         Exchanges have request limits for a period or limited
         number of request. These limits must be defined by this
-        variable in a list with predefined format such as
-        [limit number,'minutes/days/weeks/counts']
-
-        Example [4,'weeks']
+        variable. Default is 960 data per request.
         """
         raise NotImplementedError
 
