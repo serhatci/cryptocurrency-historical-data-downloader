51d906d50d093f6dff0cf0ad5e9a78792dcdebf2
serhat <serhatciftci@gmail.com>
2020-11-16 02:03:21 +0100

    Add CoinbasePro exchange

--- application/exchange_classes.py
+++ application/exchange_classes.py
@@ -15,24 +15,7 @@ class Bitpanda(Exchange):
 
     name = 'Bitpanda'
     website = 'https://www.bitpanda.com/'
-    hist_start_date = '2020-01-01 00:00:00+00:00'
     max_API_requests = 900
-    block_time_check = True
-    db_columns = [{'Column Name': 'HighPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'LowPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'OpenPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'ClosePrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'TotalAmount',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'Volume',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'Time',
-                   'Data Type': 'DATETIME'}]
-    time_col_index = 0
     api_key = None
     secret_key = None
 
@@ -55,11 +38,10 @@ class Bitpanda(Exchange):
             r = requests.get(
                 'https://api.exchange.bitpanda.com/public/v1/currencies',
                 headers=headers)
+            return str([coin['code'] for coin in r.json()]).strip('[]')
         except ConnectionError as err:
             return '\nProblem occurred while connecting to API of ' \
                    f'{self.name.upper()}\n\n{err}'
-        else:
-            return str([coin['code'] for coin in r.json()]).strip('[]')
 
     def download_hist_data(self, coin, time):
         """Downloads historical data of selected crypto asset.
@@ -100,7 +82,6 @@ class Bitpanda(Exchange):
                  data['low'],
                  data['open'],
                  data['close'],
-                 data['total_amount'],
                  data['volume']] for data in downloaded_data]
 
 
@@ -110,21 +91,7 @@ class Exmo(Exchange):
 
     name = 'Exmo'
     website = 'https://www.exmo.com'
-    hist_start_date = '2020-01-01 00:00:00+00:00'
     max_API_requests = 900
-    block_time_check = True
-    db_columns = [{'Column Name': 'Time',
-                   'Data Type': 'DATETIME'},
-                  {'Column Name': 'HighPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'LowPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'OpenPrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'ClosePrice',
-                   'Data Type': 'FLOAT(24)'},
-                  {'Column Name': 'Volume',
-                   'Data Type': 'FLOAT(24)'}]
     api_key = None
     secret_key = None
 
@@ -199,3 +166,88 @@ class Exmo(Exchange):
                  data['c'],
                  data['v']]
                 for data in downloaded_data]
+
+
+class Coinbasepro(Exchange):
+    """Creates bitpanda crypto-exchange object
+    """
+
+    name = 'CoinbasePro'
+    website = 'https://www.coinbase.com/'
+    hist_start_date = '2020-01-01 00:00:00+00:00'
+    max_API_requests = 250
+    api_key = None
+    secret_key = None
+
+    def connect_API(self) -> None:
+        """Create connection to exchange API
+
+        Returns:
+            object: API connection object
+        """
+        pass
+
+    def provide_available_coins(self):
+        """Connect exchange's API and gets all available coins.
+
+        Returns:
+            str: all available coins in the exchange
+        """
+        try:
+            r = requests.get(
+                'https://api.pro.coinbase.com/products')
+            return str([coin['id'] for coin in r.json()]).strip('[]')
+        except ConnectionError as err:
+            return '\nProblem occurred while connecting to API of '
+            f'{self.name.upper()}\n\n{err}'
+
+    def download_hist_data(self, coin, time):
+        """Downloads historical data of selected crypto asset.
+
+        Args:
+            coin (obj): given coin
+            time (list): [start date obj,end date obj]
+        """
+        def gran(freq):
+            if freq == 'minutes':
+                return '60'
+            if freq == 'hours':
+                return '3600'
+            if freq == 'days':
+                return '86400'
+            if freq == 'weeks':
+                return '604800'
+            if freq == 'months':
+                return '2419200'
+
+        link = f'https://api.pro.coinbase.com/products/' \
+            f'{coin.quote}-{coin.base}/candles'
+        data = requests.get(link, {
+            'start': time[0].shift(seconds=int(gran(coin.frequency))),
+            'end': time[1],
+            'granularity': gran(coin.frequency),
+        })
+        if not data.status_code == 200:
+            msg = f'An error was received from API of {self.name.upper()}:' \
+                f'\n\n{data.text}\n\n' \
+                'You can find more info in below link:\n' \
+                'https://docs.pro.coinbase.com/#requests'
+            raise ConnectionError(msg)
+        else:
+            return self.correct_downloaded_data(data.json())
+
+    def correct_downloaded_data(self, downloaded_data):
+        """Corrects & modifies downloaded data for cvs file.
+
+        Args:
+            downloaded_data (list): downloaded historical data
+
+        Returns:
+            list: data for csv file save
+        """
+        return [[arrow.get(data[0]).format('YYYY-MM-DD HH:mm:ss'),
+                 data[2],
+                 data[1],
+                 data[3],
+                 data[4],
+                 data[5]] for data in reversed(downloaded_data)]
