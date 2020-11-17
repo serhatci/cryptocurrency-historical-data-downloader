b8ad778babb2f0caf774287639d02512b1e85dd1
serhat <serhatciftci@gmail.com>
2020-11-18 00:26:48 +0100

    Add Kraken exchnage

--- application/exchange_classes.py
+++ application/exchange_classes.py
@@ -6,9 +6,10 @@ List of Classes:
     Coinbasepro
     Bitfinex
 """
-
+from time import sleep
 import arrow
 import requests
+import pandas as pd
 from exchange_base_cls import Exchange
 
 
@@ -200,9 +201,9 @@ class Coinbasepro(Exchange):
             str: all available coins in the exchange
         """
         try:
-            r = requests.get(
+            data = requests.get(
                 'https://api.pro.coinbase.com/products')
-            return str([coin['id'] for coin in r.json()]).strip('[]')
+            return str([coin['id'] for coin in data.json()]).strip('[]')
         except ConnectionError as err:
             return '\nProblem occurred while connecting to API of '
             f'{self.name.upper()}\n\n{err}'
@@ -215,8 +216,8 @@ class Coinbasepro(Exchange):
             time (list): [start date obj,end date obj]
         """
 
-        link = f'https://api.pro.coinbase.com/products/'
-        f'{coin.quote}-{coin.base}/candles'
+        link = f'https://api.pro.coinbase.com/products/' \
+            f'{coin.quote}-{coin.base}/candles'
         data = requests.get(link, {
             'start': time[0].shift(seconds=int(self.__gran(coin.frequency))),
             'end': time[1],
@@ -272,7 +273,7 @@ class Bitfinex(Exchange):
     name = 'Bitfinex'
     website = 'https://www.bitfinex.com'
     api_website = 'https://docs.bitfinex.com/docs/rest-general'
-    max_API_requests = 10
+    max_API_requests = 900
     api_key = None
     secret_key = None
 
@@ -291,9 +292,9 @@ class Bitfinex(Exchange):
             str: all available coins in the exchange
         """
         try:
-            r = requests.get(
+            data = requests.get(
                 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL')
-            return str([coin[0] for coin in r.json()]).strip('[]')
+            return str([coin[0] for coin in data.json()]).strip('[]')
         except ConnectionError as err:
             return '\nProblem occurred while connecting to API of '
             f'{self.name.upper()}\n\n{err}'
@@ -312,7 +313,7 @@ class Bitfinex(Exchange):
             'limit': 10,
             'start': time[0].format("x")[:13],  # convert to ms
             'end': time[1].format("x")[:13],  # convert to ms
-            'sort': '-1'})
+            'sort': '1'})
         if not data.status_code == 200:
             raise ConnectionError(self.err_msg(data.text))
         else:
@@ -353,4 +354,129 @@ class Bitfinex(Exchange):
                  data[4],
                  data[1],
                  data[2],
-                 data[5]] for data in reversed(downloaded_data)]
+                 data[5]] for data in downloaded_data]
+
+
+class Kraken(Exchange):
+    """Creates Bitfinex crypto-exchange object
+    """
+
+    name = 'Kraken'
+    website = 'https://www.kraken.com'
+    api_website = 'https://support.kraken.com/hc/en-us/articles/360001491786-API-error-messages'
+    max_API_requests = 900
+    api_key = None
+    secret_key = None
+
+    def connect_API(self) -> None:
+        """Create connection to exchange API
+
+        Returns:
+            object: API connection object
+        """
+        pass
+
+    def provide_available_coins(self):
+        """Connect exchange's API and gets all available coins.
+
+        Returns:
+            str: all available coins in the exchange
+        """
+        try:
+            data = requests.get(
+                'https://api.kraken.com/0/public/AssetPairs')
+            return str([coin for coin in data.json()['result']]).strip('[]')
+        except ConnectionError as err:
+            return '\nProblem occurred while connecting to API of '
+            f'{self.name.upper()}\n\n{err}'
+
+    def download_hist_data(self, coin, time):
+        """Downloads historical data of selected crypto asset.
+
+        Kraken has a different API than others. User gives a start date and
+        API provides all trades with resolution in seconds up 720 data
+        points! Than app should select the last date of result and start
+        requesting next query by using the last date of previous query.
+        That is why this method has additional data process method.
+        for more info:
+        https://support.kraken.com/hc/en-us/articles/218198197
+
+        Args:
+            coin (obj): given coin
+            time (list): [start date obj,end date obj]
+        """
+        link = f'https://api.kraken.com/0/public/Trades'
+        final_data = []
+        while time[0] < time[1]:
+            data = requests.get(link, params={
+                'pair': f'{coin.quote}{coin.base}',
+                'since': time[0].format('X')})
+            if not data.status_code == 200 or data.json()['error'] != []:
+                raise ConnectionError(self.err_msg(data.json()['error']))
+            else:
+                processed_data = self.__process_data(data)
+                for item in processed_data:
+                    final_data.append(item)
+                time[0] = arrow.get(int(data.json()['result']['last'][:10]))
+                sleep(1.5)
+
+        return self.correct_downloaded_data(final_data)
+
+    @staticmethod
+    def __process_data(data):
+        """Granulate the data from seconds to minutes
+
+        Arg:
+            data (list): donwloaded data
+
+        Return:
+            (list): list of granulated data
+        """
+        for data in data.json()['result'].values():
+            result = [[float(i[0]),
+                       float(i[1]),
+                       arrow.get(i[2]).format()]
+                      for i in data]
+            break
+        df = pd.DataFrame(result, columns=['price', 'vol', 'time'])
+        df['time'] = pd.to_datetime(df['time'])
+        df2 = df.resample('1min', on='time').mean().fillna(0)  # granulates
+        df2.reset_index(inplace=True)
+        return df2.values.tolist()
+
+    @ staticmethod
+    def __gran(freq):
+        """Provides exchange specific granularity.
+
+        Args:
+            freq (str): frequency given by user
+
+        Returns:
+            str: appropriate granularity
+        """
+        if freq == 'minutes':
+            return '1'
+        if freq == 'hours':
+            return '60'
+        if freq == 'days':
+            return '1440'
+        if freq == 'weeks':
+            return '10080'
+        if freq == 'months':
+            return '40320'
+
+    def correct_downloaded_data(self, downloaded_data):
+        """Corrects & modifies downloaded data for cvs file.
+
+        Args:
+            downloaded_data (list): downloaded historical data
+
+        Returns:
+            list: data for csv file save
+        """
+        return [[arrow.get(data[0]).format('YYYY-MM-DD HH:mm:ss'),
+                 '-',
+                 '-',
+                 '-',
+                 round(data[1], 2),
+                 data[2]] for data in downloaded_data]
