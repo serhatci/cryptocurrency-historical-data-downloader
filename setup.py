cec5d7d62b35591d9d0655461b032f9bb83ca54f
serhat <serhatciftci@gmail.com>
2020-11-22 20:00:00 +0100

    Update for PyPI upload

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
+import pathlib
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="cryptoasset-data-downloader",
-    version="0.0.1",
+    version="1.0.0",
     author="Serhat",
     author_email="start.a.huge.foolish.project@gmail.com",
     description="A desktop application to download historical data" +
     "of desired crypto assets by connecting several different" +
     "crypto-exchanges' API",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/serhatci/cryptocurrency-historical-data-downloader",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests')),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['pandas','arrow','request'],
+    entry_points={
+        "console_scripts": [
+            "cryptoasset-data-downloader=application.__main__:main",
+        ]
+    },
 )
