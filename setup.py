fc149a96ba95ad8ea931564b0ef4d3f9b470253e
serhat <serhatciftci@gmail.com>
2020-11-22 22:00:08 +0100

    Update packaging info

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/serhatci/cryptocurrency-historical-data-downloader",
-    packages=setuptools.find_packages(exclude=('tests')),
+    packages=['application'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
