84b030b1871f0c7ba8bdc00c2d7d179b7f763e29
serhat <serhatciftci@gmail.com>
2020-11-23 12:18:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,26 +9,26 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="cryptoasset-data-downloader",
-    version="1.0.0",
+    version="1.0.8",
     author="Serhat",
     author_email="start.a.huge.foolish.project@gmail.com",
-    description="A desktop application to download historical data" +
-    "of desired crypto assets by connecting several different" +
+    description="A desktop application to download historical data " +
+    "of desired crypto assets by connecting several different " +
     "crypto-exchanges' API",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/serhatci/cryptocurrency-historical-data-downloader",
-    packages=['application'],
+    packages=['application', 'application/classes'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['pandas','arrow','request'],
+    install_requires=['pandas', 'arrow', 'requests', 'PySimpleGUI'],
     entry_points={
         "console_scripts": [
-            "cryptoasset-data-downloader=application.__main__:main",
+            "cryptoasset-data-downloader=application.main:run",
         ]
     },
 )
